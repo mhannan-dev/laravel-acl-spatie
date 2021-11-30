53e1c16fd6d780da559a1da2d7f2d8b2368e492c
M Hannan <mdhannan.info@gmail.com>
2021-11-30 20:17:14 +0600

    Set Admin For ACL

--- app/Http/Controllers/Backend/DashboardControl.php
+++ app/Http/Controllers/Backend/DashboardControl.php
@@ -1,8 +1,7 @@
 <?php
 
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
+namespace App\Http\Controllers\Backend;
+use App\Http\Controllers\Controller;
 
 class DashboardControl extends Controller
 {
