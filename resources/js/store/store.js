d24a7f26316cc6cbb9716f7e386a7a4f089e0e69
Muhammad Hannan <mdhannan.info@gmail.com>
2022-06-23 23:53:18 +0600

    Set vues

--- resources/js/store/store.js
+++ resources/js/store/store.js
@@ -1,19 +1,47 @@
 import axios from "axios";
-export default{
-    state:{
-        category:[],
-        user:[],
-        content:[]
+export default {
+    state: {
+        category: [],
+        user: [],
+        content: []
     },
     getters: {
-        categoryList (state) {
-          return state.category
+        categoryList(state) {
+            return state.category
         },
-        userList (state) {
-          return state.user
+        userList(state) {
+            return state.user
         },
-        contentList (state) {
-          return state.content
+        contentList(state) {
+            return state.content
         }
-      }
+    },
+    actions: {
+        categoryList(context) {
+            axios.get('/categoryList').then((response) =>{
+                context.commit('categoryList',response.data.categoryList)
+            })
+        },
+        userList(context) {
+            axios.get('/userList').then((response) =>{
+                context.commit('userList',response.data.userList)
+            })
+        },
+        contentList(context) {
+            axios.get('/contentList').then((response) =>{
+                context.commit('contentList',response.data.contentList)
+            })
+        }
+    },
+    mutations:{
+        categoryList(state, response) {
+            return state.category = response
+        },
+        userList(state, response) {
+            return state.user = response
+        },
+        contentList(state, response) {
+            return state.content = response
+        },
+    }
 }
